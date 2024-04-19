931e2c84e00843bd1c6a743e837697500cd896b1
fernandoruiz <fernando.ruiz@dicsys.com>
2024-04-19 19:54:04 -0300

    feat-Creacion de paginas

---
+++ src/app/heroes-detail/heroes-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-detail',
+  templateUrl: './heroes-detail.page.html',
+  styleUrls: ['./heroes-detail.page.scss'],
+})
+export class HeroesDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
